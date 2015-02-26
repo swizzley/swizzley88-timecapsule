a00cff1fc2672bc8c1c23b582902f88f02e6b2d7
Morgan <dmorgan@oildex.com>
2015-02-25 21:57:31 -0700

    spec helper

---
+++ spec/timecapsule_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'timecapsule', :type => 'class' do
+
+  context "On a RedHat 7 OS" do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+        :operatingsystemmajrelease => '7'
+      }
+    end
+
+    it {
+      should contain_package('netatalk').with( { 'name' => 'netatalk' } )
+      should contain_service('netatalk').with( { 'name' => 'netatalk' } )
+      should contain_package('avahi').with( { 'name' => 'avahi' } )
+      should contain_service('avahi-daemon').with( { 'name' => 'avahi-daemon' } )
+      should contain_package('nss-mdns').with( { 'name' => 'nss-mdns' } )
+      should contain_service('messagebus').with( { 'name' => 'messagebus' } )
+      should contain_package('dbus').with( { 'name' => 'dbus' } )
+    }
+  end
+
+  context "On an unknown OS with no package name specified" do
+    let :facts do
+      {
+        :osfamily => 'Darwin'
+      }
+    end
+
+    it {
+      expect { should raise_error(Puppet::Error) }
+    }
+  end
