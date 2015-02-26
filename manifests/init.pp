15a9492eb6bbcd3c0d74c86fdea7586dee6a37c5
Morgan <dmorgan@oildex.com>
2015-02-25 21:57:50 -0700

    OS case for RHEL only

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,118 +43,124 @@ class timecapsule (
   validate_bool($manage_group)
   validate_bool($gpgcheck)
 
-  if $install_epel {
-    exec { 'install EPEL 7 repo':
-      path    => '/usr/bin',
-      command => "yum install -y ${repo}",
-      unless  => 'test -f /etc/yum.repos.d/epel.repo',
+  case $::osfamily {
+    'RedHat' : {
+      if $install_epel {
+        exec { 'install EPEL 7 repo':
+          path    => '/usr/bin',
+          command => "yum install -y ${repo}",
+          unless  => 'test -f /etc/yum.repos.d/epel.repo',
+        }
+
+        yumrepo { 'epel':
+          descr          => 'Extra Packages for Enterprise Linux 7 - x86_64',
+          mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=x86_64',
+          baseurl        => 'http://download.fedoraproject.org/pub/epel/7/x86_64',
+          failovermethod => 'priority',
+          enabled        => true,
+          gpgcheck       => $gpgcheck,
+          gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
+        }
+
+        file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7':
+          ensure => present,
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0644',
+          source => 'puppet:///modules/timecapsule/RPM-GPG-KEY-EPEL-7',
+        }
+      }
+
+      if $::operatingsystem != 'Fedora' {
+        exec { 'get netatalk package from rpmfind ftp':
+          path    => '/usr/bin',
+          command => "yum install -y ${netatalk_url}",
+          unless  => 'test -f /usr/sbin/afpd',
+        }
+      }
+
+      package { $package: ensure => installed } ->
+      service { $services:
+        ensure => 'running',
+        enable => true,
+      }
+
+      file { $mount:
+        ensure => directory,
+        owner  => $user,
+        group  => $group,
+        mode   => '0755',
+      }
+
+      file { '/etc/netatalk/afpd.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => $afpd,
+        require => Package['netatalk'],
+        notify  => Service['netatalk'],
+      }
+
+      file { '/etc/netatalk/AppleVolumes.default':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('timecapsule/AppleVolumes.default.erb'),
+        require => Package['netatalk'],
+        notify  => Service['netatalk'],
+      }
+
+      file { '/etc/avahi/services/afpd.service':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('timecapsule/afpd.service.erb'),
+        require => Package['avahi'],
+        notify  => Service['avahi-daemon'],
+      }
+
+      exec { 'nsswitch switch':
+        path    => '/usr/bin',
+        command => 'cp /etc/nsswitch.conf /etc/nsswitch.conf.pre-timecapsule && sed -i "/^hosts:/c\hosts:      files mdns4_minimal dns mdns mdns4" /etc/nsswitch.conf',
+        unless  => 'test -f /etc/nsswitch.conf.pre-timecapsule',
+      }
+
+      if $bonjour_ssh {
+        file { $avahi_ssh:
+          ensure => present,
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0644',
+        }
+      } else {
+        file { $avahi_ssh: ensure => absent, }
+      }
+
+      if $firewall {
+        firewall { '000 accept all timecapsule':
+          port   => $ports,
+          proto  => ['tcp', 'udp'],
+          action => 'accept',
+        }
+      }
+
+      if $manage_user {
+        user { $user:
+          ensure   => present,
+          gid      => $group,
+          password => $pass,
+        }
+      }
+
+      if $manage_group {
+        group { $group: ensure => present, }
+      }
     }
-
-    yumrepo { 'epel':
-      descr          => 'Extra Packages for Enterprise Linux 7 - x86_64',
-      mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=x86_64',
-      baseurl        => 'http://download.fedoraproject.org/pub/epel/7/x86_64',
-      failovermethod => 'priority',
-      enabled        => true,
-      gpgcheck       => $gpgcheck,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
-    }
-
-    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/timecapsule/RPM-GPG-KEY-EPEL-7',
+    default  : {
+      fail("unsupported osfamily: $::osfamily")
     }
   }
-
-  if $::operatingsystem != 'Fedora' {
-    exec { 'get netatalk package from rpmfind ftp':
-      path    => '/usr/bin',
-      command => "yum install -y ${netatalk_url}",
-      unless  => 'test -f /usr/sbin/afpd',
-    }
-  }
-
-  package { $package: ensure => installed } ->
-  service { $services:
-    ensure => 'running',
-    enable => true,
-  }
-
-  file { $mount:
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-    mode   => '0755',
-  }
-
-  file { '/etc/netatalk/afpd.conf':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => $afpd,
-    require => Package['netatalk'],
-    notify  => Service['netatalk'],
-  }
-
-  file { '/etc/netatalk/AppleVolumes.default':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('timecapsule/AppleVolumes.default.erb'),
-    require => Package['netatalk'],
-    notify  => Service['netatalk'],
-  }
-
-  file { '/etc/avahi/services/afpd.service':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('timecapsule/afpd.service.erb'),
-    require => Package['avahi'],
-    notify  => Service['avahi-daemon'],
-  }
-
-  exec { 'nsswitch switch':
-    path    => '/usr/bin',
-    command => 'cp /etc/nsswitch.conf /etc/nsswitch.conf.pre-timecapsule && sed -i "/^hosts:/c\hosts:      files mdns4_minimal dns mdns mdns4" /etc/nsswitch.conf',
-    unless  => 'test -f /etc/nsswitch.conf.pre-timecapsule',
-  }
-
-  if $bonjour_ssh {
-    file { $avahi_ssh:
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-    }
-  } else {
-    file { $avahi_ssh: ensure => absent, }
-  }
-
-  if $firewall {
-    firewall { '000 accept all timecapsule':
-      port   => $ports,
-      proto  => ['tcp', 'udp'],
-      action => 'accept',
-    }
-  }
-
-  if $manage_user {
-    user { $user:
-      ensure   => present,
-      gid      => $group,
-      password => $pass,
-    }
-  }
-
-  if $manage_group {
-    group { $group: ensure => present, }
-  }
-
 }
